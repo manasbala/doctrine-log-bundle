820a49caee0796386919c25f9a110a70a03ede5e
Michael Dawart <md@neuewerte.de>
2021-09-15 14:21:11 +0200

    Add back compatibility for symfony/config 4.1 and older https://github.com/KnpLabs/DoctrineBehaviors/pull/411/commits/fb693d9dc258695010e5d64ca6a3bdff38090065

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,7 +17,12 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
 	    $treeBuilder = new TreeBuilder('mb_doctrine_log');
-	    $rootNode = $treeBuilder->getRootNode();
+	    if (method_exists($treeBuilder, 'getRootNode')) {
+		    $rootNode = $treeBuilder->getRootNode();
+	    } else {
+		    // for symfony/config 4.1 and older
+		    $rootNode = $treeBuilder->root('mb_doctrine_log');
+	    }
 
         $rootNode
             ->children()
