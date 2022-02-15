63670385529d52e2852a0ea6b1f56ea0c716e76a
mdawart <mdawart@users.noreply.github.com>
2022-02-15 06:42:34 +0100

    Add fix for Symfony 5 / Config Tree Builder issue (#6)
    * Add fix for Symfony 5 / Config Tree Builder issue
    
    * Add back compatibility for symfony/config 4.1 and older
    https://github.com/KnpLabs/DoctrineBehaviors/pull/411/commits/fb693d9dc258695010e5d64ca6a3bdff38090065

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -16,8 +16,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('mb_doctrine_log');
+	    $treeBuilder = new TreeBuilder('mb_doctrine_log');
+	    if (method_exists($treeBuilder, 'getRootNode')) {
+		    $rootNode = $treeBuilder->getRootNode();
+	    } else {
+		    // for symfony/config 4.1 and older
+		    $rootNode = $treeBuilder->root('mb_doctrine_log');
+	    }
 
         $rootNode
             ->children()
